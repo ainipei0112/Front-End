35c9bda0198f8a98a34f174be12ca4c20e570c59
ainipei0112 <ainipei0112@gmail.com>
2021-06-10 17:54:00 +0800

    Update App.js

--- 0527/App.js
+++ 0527/App.js
@@ -12,6 +12,14 @@ import TextField from '@material-ui/core/TextField';
 import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 
 import { green, orange } from '@material-ui/core/colors'
+import '@fontsource/roboto';
+
+import Typography from '@material-ui/core/Typography';
+
+import Appbar from '@material-ui/core/Appbar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles({
   root: {
@@ -25,14 +33,11 @@ const useStyles = makeStyles({
 })
 
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: green[400],
-    },
-    secondary: {
-      main: orange[400],
+  typography: {
+    h2: {
+      fontSize: 36,
     }
-  }
+  },
 })
 
 function ButtonStyled() {
@@ -63,6 +68,25 @@ function App() {
     <ThemeProvider theme={theme}>
     <div className="App">
       <header className="App-header">
+	<Appbar color="secondary">
+	  <Toolbar>
+	    <IconButton>
+	      <MenuIcon />
+	    </IconButton>
+	    <Typography variant="h6">
+	 	MUI Themeing
+	    </Typography>
+	    <Button>
+	      Login
+	    </Button>
+	  </Toolbar>
+	</Appbar>
+	<Typography variant="h2" component="div">
+	 Welcome to MUI
+	</Typography>
+	<Typography variant="subtitle1">
+	 Learnhow to use Material Ui
+	</Typography>
 	<ButtonStyled />
 	<TextField
 	 variant="filled" 
